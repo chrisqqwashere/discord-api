56fad2493e6374464f1efd0674ff90c22ce5501f
Chris0666 <77915141+Chris0666@users.noreply.github.com>
2021-03-23 13:48:40 +0300

    Update helper.js

--- src/chrisqq/helper.js
+++ src/chrisqq/helper.js
@@ -24,5 +24,22 @@ class Chrisqq {
       }
     );
   }
+  avatar(id) {
+        request(
+      {
+        url: "https://discordapp.com/api/v7/users/" + id,
+        headers: {
+          Authorization: `Bot ${this.token}`,
+        },
+      },
+      function (err, response, body) {
+        if (err) console.log(err);
+        if (!body) return;
+        if (body)
+          console.log(chalk.green(id + " adlı kullanıcı başarıyla çekildi ✅"));
+          console.log(chalk.red("Avatar : ") + chalk.yellow("https://cdn.discordapp.com/avatars/"+id+"/"+body.avatar+".png?size=128"))
+      }
+    );
+  }
 }
 module.exports = Chrisqq;
